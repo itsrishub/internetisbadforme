266e9992cacbc2ef8a74e72df5a5c9ec821ec3a3
xy0ke <48987167+xy0ke@users.noreply.github.com>
2021-04-12 22:21:20 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,3 @@
-
-
-
-
 from flask import Flask
 import cloudscrapper
 import requests
@@ -10,19 +6,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  email = input("Enter your email\n\n")
-
-  scraper = cloudscraper.create_scraper()
-  jsonGet = scraper.get(f"https://haveibeenpwned.com/unifiedsearch/{email}").text
-  jsonData = json.loads(jsonGet)
-
-  site = ""
-
-  for data in jsonData['Breaches']:
-    site = data['Title']
-    if data['Title']:
-      print(data['Title'])
-     else:
-      print("Your email is safe!\n\n")
-      break
-   input("\nEnter to exit\n")
+  print("hello")
