36fbdc7553900c21aaf0f4ece145de5a1f5a83ce
xy0ke <48987167+xy0ke@users.noreply.github.com>
2021-04-13 22:36:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,7 @@
- from flask import Flask, request
- from datetime import datetime
+from flask import Flask
 
- app = Flask(__name__)
+app = Flask(__name__)
 
- @app.route('/')
- def hello():
-     name = request.args['name']
-     return HELLO_HTML.format(
-             name, str(datetime.now()))
-
- HELLO_HTML = """
-     <html><body>
-         <h1>Hello, {0}!</h1>
-         The time is {1}.
-     </body></html>"""
-
- if __name__ == "__main__":
-     # Launch the Flask dev server
-     app.run(host="localhost", debug=True)
+@app.route("/")
+def home_view():
+    return "<h1>hello bggggdk</h1>"
