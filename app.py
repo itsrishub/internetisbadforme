450332cf9aa047910ef2ff8c31a9c41516ffcc0e
xy0ke <48987167+xy0ke@users.noreply.github.com>
2021-04-13 23:23:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,12 @@
+from flask import Flask
 
-# importing Flask and other modules
-from flask import Flask, request, render_template 
-  
-# Flask constructor
-app = Flask(__name__)   
-  
-# A decorator used to tell the application
-# which URL is associated function
-@app.route('/', methods =["GET", "POST"])
-def gfg():
-    if request.method == "POST":
-       # getting input with name = fname in HTML form
-       first_name = request.form.get("fname")
-       # getting input with name = lname in HTML form 
-       last_name = request.form.get("lname") 
-       return "Your name is "+first_name + last_name
-    return render_template("form.html")
-  
-if __name__=='__main__':
-   app.run()
+app = Flask(__name__)
+
+@app.route("/")
+def home_view():
+    return '<form action="#" method="post">
+<label for="firstname">First Name:</label>
+<input type="text" id="firstname" name="fname" placeholder="firstname">
+<label for="lastname">Last Name:</label>
+<input type="text" id="lastname" name="lname" placeholder="lastname">
+<button type="submit">Login</button>''
