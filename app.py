4857bc19b89efda341aa3b79a45f97ffaa88f85e
xy0ke <48987167+xy0ke@users.noreply.github.com>
2021-04-13 23:02:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 def student():
    return render_template('form.html')
 
-@app.route('/',methods = ['POST', 'GET'])
-def result():
-   if request.method == 'POST':
-      result = request.form
-      return render_template("result.html",result = result)
+# @app.route('/',methods = ['POST', 'GET'])
+# def result():
+#    if request.method == 'POST':
+#       result = request.form
+#       return render_template("result.html",result = result)
 
 if __name__ == '__main__':
    app.run(debug = True)
