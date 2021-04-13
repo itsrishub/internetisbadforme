fdc3c436913fda53fa3bc2868bc2c749e1651bbf
xy0ke <48987167+xy0ke@users.noreply.github.com>
2021-04-13 23:25:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_view():
-    return '<form action="#" method="post">
-<label for="firstname">First Name:</label>
-<input type="text" id="firstname" name="fname" placeholder="firstname">
-<label for="lastname">Last Name:</label>
+    return '<form action="#" method="post"><label for="firstname">First Name:</label><input type="text" id="firstname" name="fname" placeholder="firstname">
+<label for="lastname">Last Name</label>
 <input type="text" id="lastname" name="lname" placeholder="lastname">
-<button type="submit">Login</button>''
+<button type="submit">Login</button>'
