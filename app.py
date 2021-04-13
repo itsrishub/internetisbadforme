84e95e9393a880f8b32acd5b1ca2c8b9a29c7096
xy0ke <48987167+xy0ke@users.noreply.github.com>
2021-04-13 23:04:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def student():
-   return render_template('form.html')
-
-# @app.route('/',methods = ['POST', 'GET'])
-# def result():
-#    if request.method == 'POST':
-#       result = request.form
-#       return render_template("result.html",result = result)
-
-if __name__ == '__main__':
-   app.run(debug = True)
+@app.route("/")
+def home_view():
+    return render_template('form.html')
