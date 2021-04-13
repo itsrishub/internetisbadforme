34fe96b746786515c55de1fa15fa4c8e181ded31
xy0ke <48987167+xy0ke@users.noreply.github.com>
2021-04-13 22:14:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,7 @@
 from flask import Flask
-import cloudscraper
-import requests
-import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_view():
-	email = input("Enter email\n\n")
-
-	scraper = cloudscraper.create_scraper()
-	jsonGet = scraper.get(f"https://haveibeenpwned.com/unifiedsearch/rishab@gmail.com").text
-	jsonData = json.loads(jsonGet)
-
-	site = ""
-
-	for data in jsonData['Breaches']:
-		site = data['Title']
-		if data['Title']:
-			print(data['Title'])
-		else:
-			print("Your email isn't breached!")
-			break
-
-	input("\n\n\n\nenter to exit...")
+    return "<h1>hello bsdk</h1>"
